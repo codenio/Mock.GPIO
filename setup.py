5d09e9b6f58ad15a945bea91df373f0d99eead97
codenio <aananthraj1995@gmail.com>
2025-09-29 13:45:57 +0530

    update docs

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from distutils.util import convert_path
 
 from setuptools import setup, find_packages
@@ -5,48 +6,7 @@ from setuptools import setup, find_packages
 # Copyright (c) 2025 codenio ( Aananth K )
 # SPDX-License-Identifier: GPL-3.0-only
 
-
-long_description = """
-# GPIOEmulator
-
-The easiest way to use this package is to install using pip3 for python 3
-
-```bash
-$ sudo pip3 install Mock.GPIO
-```
-
-To use the emulator just type the following at the beginning of your script.
-
-```python
-from Mock.GPIO import GPIO
-```
-
-## Works with
-
-- [python 3.6.8](https://www.python.org/downloads/release/3.6.8)
-
-## Simulation
-
-This library simulates the following functions which are used in the RPi.GPIO library.
-
-- GPIO.setmode()
-- GPIO.getmode()
-- GPIO.setwarnings()
-- GPIO.setup()
-- GPIO.output()
-- GPIO.input()
-- GPIO.wait_for_edge()
-- GPIO.add_event_detect()
-- GPIO.event_detected()
-- GPIO.add_event_callback()
-- GPIO.remove_event_detect()
-- GPIO.gpio_function()
-- GPIO.start()
-- GPIO.ChangeFrequency()
-- GPIO.ChangeDutyCycle()
-- GPIO.stop()
-- GPIO.cleanup()
-"""
+readme = Path(__file__).with_name("README.md").read_text(encoding="utf-8")
 
 pkg_ns = {}
 
@@ -58,7 +18,7 @@ setup(
     name="Mock.GPIO",
     version=pkg_ns["__version__"],
     description="Mock Library for RPi.GPIO",
-    url="https://github.com/codenio/",
+    url="https://github.com/codenio/Mock.GPIO",
     author="Aananth K",
     author_email="aananthraj1995@gmail.com",
     license="GPL-3.0",
@@ -66,5 +26,5 @@ setup(
     install_requires=[],
     zip_safe=False,
     long_description_content_type="text/markdown",
-    long_description=long_description,
+    long_description=readme,
 )
